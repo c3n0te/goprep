ad39996921bd5031a4dbcb752f3b8b550d484742
c3n0te <tyellison95@gmail.com>
2026-08-11 21:07:50 -0700

    adding goroutine prep

---
+++ thread/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func helloWorld() string {
+	return "hello world"
+}
+
+func downloadImage(url string, fname string, ch chan<- string, idx int) {
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- "Error querying image url"
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		ch <- fmt.Sprintf("Returned status code: %v", resp.StatusCode)
+		return
+	}
+
+	imgF, err := os.Create(fname)
+	if err != nil {
+		ch <- "Error opening file"
+		return
+	}
+	defer imgF.Close()
+
+	_, err = io.Copy(imgF, resp.Body)
+	if err != nil {
+		ch <- "Error copying responde body into file"
+		return
+	}
+
+	ch <- "Successfully downloaded image"
+}
+
+func main() {
+	urls := []string{
+		"https://go.dev/images/gophers/blue.svg",
+		"https://go.dev/images/gophers/motorcycle.svg",
+		"https://go.dev/images/gophers/machine.svg",
+		"https://go.dev/images/gophers/skateboarding.svg",
+	}
+
+	ch := make(chan string)
+	for i, url := range urls {
+		go downloadImage(url, fmt.Sprintf("./data/downloaded_image_%v.svg", i), ch, i)
+	}
+
+	for range urls {
+		fmt.Println(<-ch)
+	}
+}
---
+++ thread/main_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestHelloWorld(t *testing.T) {
+	s := helloWorld()
+	expected := "hello world"
+	if s != expected {
+		t.Errorf("Expected string did not match")
+	}
+}
+
+func TestDownloadImage(t *testing.T) {
+	ch := make(chan string)
+	go downloadImage("https://go.dev/images/gophers/skateboarding.svg", fmt.Sprintf("../data/downloaded_image_%v.svg", 1), ch, 1)
+	successStr := <-ch
+	if successStr != "Successfully downloaded image" {
+		t.Errorf("Failed to download image")
+	}
+}
